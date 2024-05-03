b89ff653b5fddb764751568ea73489b54f4a733c
PatrickVigramer <patrick.kranzinger@vigram.com>
2024-05-03 14:05:06 +0200

    Switch to prepared xcframwork of swift-syntax

--- Package.swift
+++ Package.swift
@@ -47,8 +47,7 @@ let package = Package(
         .macro(
             name: "ObfuscateMacroPlugin",
             dependencies: [
-                .product(name: "SwiftSyntaxWrapper", package: "framework-ios-swift-syntax"),
-                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxWrapper", package: "swift-syntax-xcframeworks"),                .product(name: "SwiftParser", package: "swift-syntax"),
                 .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
                 .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "Crypto", package: "swift-crypto"),
